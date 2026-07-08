0b790fc2cd1a8c594df33d3b17a8e450c35dcb51
Carlos Gordillo <cgordillomolina@gmail.com>
2026-07-08 17:40:16 -0500

    Supabase y API RESTful.

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from models.form_data import FormData
 from typing import Annotated
 from fastapi import FastAPI, Form, Response
 from fastapi import FastAPI, Form, Response, HTTPException, status
+from models.task import Task
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_PUBLISHABLE_KEY"))
 
 app = FastAPI()
 
@@ -76,3 +83,16 @@ def create_item(
    fake_items_db.append(item_name)
 
    return Response(content=message, status_code=201)
+
+@app.post("/tasks/")
+def create_task(task: Task):
+  data = supabase.table("task").insert({
+      "title": task.title,
+      "description": task.description
+  }).execute()
+  return data.data
+
+@app.get("/tasks/")
+def get_tasks():
+   data = supabase.table("task").select("*").execute()
+   return data.data
